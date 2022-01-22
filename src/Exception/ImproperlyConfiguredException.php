5bf544c254e8738d2cbb4c5589b90655c89cd24a
ncou <nicolas_courade@yahoo.fr>
2022-01-22 15:11:09 +0100

    update

---
+++ src/Exception/ImproperlyConfiguredException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Core\Exception;
+
+// Chiron is somehow improperly configured.
+class ImproperlyConfiguredException extends \RuntimeException
+{
+}
